0b87bfbba0dbfdf700541af3d172eb3472651ac9
vincent <vincent@backbonesystems.nl>
2017-05-20 14:51:18 +0200

    Added planet dialog

---
+++ Assets/Scripts/Planet/Dialog/PlanetDialog.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetDialog : MonoBehaviour {
+
+	public GameObject planet;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);
+
+		// Draw a triangle to connect the dialog to the planet
+//		GameObject m_goTriangle = new GameObject();
+//		m_goTriangle.AddComponent<MeshFilter>();
+//		m_goTriangle.AddComponent<MeshRenderer>();
+//		Mesh m_meshTriangle = m_goTriangle.GetComponent<MeshFilter>().mesh;
+//		m_meshTriangle.Clear();
+//		Vector3 bottom = planetScreenCoordinate;
+//		bottom.y -= 40;
+//		Vector3 left = planetScreenCoordinate;
+//		left.x -= 5;
+//		left.y -= 25;
+//		Vector3 right = planetScreenCoordinate;
+//		right.x += 5;
+//		right.y -= 25;
+//		m_meshTriangle.vertices = new Vector3[] { bottom, left, right };
+//		m_meshTriangle.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 0.25f), new Vector2(0.25f, 0.25f) };
+//		m_meshTriangle.triangles = new int[] { 0, 1, 2 };
+////		Graphics.DrawMesh(m_meshTriangle, Vector3.zero, Quaternion.identity, newMat, 0);
+//
+//
+//		Material material = new Material (Shader.Find("UI/Default"));
+//		material.SetColor("_TintColor", Color.white);
+//
+//		CanvasRenderer pCanvasRenderer = gameObject.GetComponent<CanvasRenderer>();
+//
+//		RectTransform pRectTransform = gameObject.GetComponent<RectTransform>();
+//		if(pRectTransform == null)
+//		{
+//			Vector3 pos = gameObject.transform.localPosition;
+//			Quaternion rot = gameObject.transform.localRotation;
+//			Vector3 scale = gameObject.transform.localScale;
+//
+//			pRectTransform = gameObject.AddComponent<RectTransform>();
+//			pRectTransform.localPosition = pos;
+//			pRectTransform.localRotation = rot;
+//			pRectTransform.localScale = scale;
+//		}
+//
+//		pCanvasRenderer = gameObject.AddComponent<CanvasRenderer>();
+//
+//		UIVertex uiVertexBottom = new UIVertex ();
+//		uiVertexBottom.position = bottom;
+//		UIVertex uiVertexLeft = new UIVertex ();
+//		uiVertexLeft.position = left;
+//		UIVertex uiVertexRight = new UIVertex ();
+//		uiVertexRight.position = right;
+//		List<UIVertex> vertices = new List<UIVertex> ();
+//		vertices.Add (uiVertexBottom);
+//		vertices.Add (uiVertexLeft);
+//		vertices.Add (uiVertexRight);
+//
+//		pCanvasRenderer.Clear();
+//		pCanvasRenderer.SetMaterial(material, null);
+//		pCanvasRenderer.SetVertices(vertices);
+	}
+
+	public void UpdatePosition(GameObject planet) {
+		gameObject.SetActive (true);
+
+		// Put the dialog above the planet
+		Vector3 planetScreenCoordinate = Camera.main.WorldToScreenPoint (planet.transform.position);
+		planetScreenCoordinate.y += 50;
+		gameObject.transform.position = planetScreenCoordinate;
+	}
+
+	public void Hide() {
+		gameObject.SetActive (false);
+	}
+}
--- Assets/Scripts/Planet/PlanetClickBehaviour.cs
+++ Assets/Scripts/Planet/PlanetClickBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetClickBehaviour : MonoBehaviour {
-	public Camera camera;
+	public GameObject planetClickDialog;
 
 	void Start() {
 	}
@@ -16,12 +16,16 @@ public class PlanetClickBehaviour : MonoBehaviour {
 			RaycastHit2D hitInfo = Physics2D.Raycast (worldPoint, Vector2.zero);
 			if (hitInfo) {
 				Debug.Log ("Object Hit is 2D " + hitInfo.collider.gameObject.name);
+				PlanetDialog planetDialog = (PlanetDialog) planetClickDialog.GetComponent(typeof(PlanetDialog));
+				planetDialog.UpdatePosition (hitInfo.collider.gameObject);
 			} else {
 				Debug.Log ("Nothing Hit is 2D ");
+				PlanetDialog planetDialog = (PlanetDialog) planetClickDialog.GetComponent(typeof(PlanetDialog));
+				planetDialog.Hide ();
 			}
 
 			RaycastHit hit;
-			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast (ray, out hit))
 			if (hit.collider != null) {
 				hit.collider.transform.localScale += new Vector3(0.1F, 0.1F, 0.1F);
